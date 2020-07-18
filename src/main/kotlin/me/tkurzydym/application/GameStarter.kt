448fcde4fb8cb64b3d41f1f9622e23b6a60d145a
tkurzydym <tobias.kurzydym@protonmail.com>
2020-07-18 16:10:43 +0200

    Added a Game Starter

---
+++ src/main/kotlin/me/tkurzydym/application/GameStarter.kt
@@ -0,0 +1,15 @@
+package me.tkurzydym.application
+
+import me.tkurzydym.application.game.KoPSR
+import me.tkurzydym.model.player.Player
+import me.tkurzydym.model.player.RandomPlayer
+import me.tkurzydym.model.player.RockThrower
+
+fun main() {
+    val kopsr = KoPSR()
+    kopsr.playGame(
+        rounds =    100,
+        p1 = Player(name = "Player1", capabilities = RandomPlayer()),
+        p2 = Player(name = "Rockthrower", capabilities = RockThrower())
+    )
+}
