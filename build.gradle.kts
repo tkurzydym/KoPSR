1719e26bccfcfd364abe53eb98dbad66e2d790a6
tkurzydym <tobias.kurzydym@protonmail.com>
2020-07-18 11:24:48 +0200

    add junit and kotest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,14 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.1.1") // for kotest framework
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.1.1") // for kotest core jvm assertions
+    testImplementation("io.kotest:kotest-property-jvm:4.1.1") // for kotest property test
+    testImplementation("io.mockk:mockk:1.10.0")
+    testImplementation("io.kotest:kotest-runner-console-jvm:4.1.1")
 }
 
 tasks {
@@ -19,5 +27,8 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+    test {
+        useJUnitPlatform()
     }
 }
